d510138284bb0da0b68ccb5c5be9f724924505e5
27Ekaterina <103840123+27Ekaterina@users.noreply.github.com>
2022-09-10 13:32:33 +0300

    Initial commit

---
+++ mysite/blog/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.core.validators import FileExtensionValidator
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Video(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    create = models.DateTimeField(auto_now_add=True)
+    file = models.FileField(
+        upload_to='video/', null=True, blank=True, unique=True,
+        validators=[FileExtensionValidator(allowed_extensions=['mp4'])]
+    )
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
+
+
